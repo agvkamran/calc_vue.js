fd0d090df90db541b86a9675eedfc6646de96eaa
Kamran Aghayev <aghayev189@gmail.com>
2020-06-30 22:02:47 +0400

    calculator_vue4

---
+++ script.js
@@ -0,0 +1,20 @@
+let app = new Vue({
+  el: "#app",
+  data: {
+    result: "",
+    numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
+    operations: ["+", "-", "*", "/"],
+  },
+  methods: {
+    input: function (char) {
+      this.result += char;
+      this.result = this.result.toString();
+    },
+    reset: function () {
+      this.result = "";
+    },
+    calc: function () {
+      this.result = eval(this.result);
+    },
+  },
+});
